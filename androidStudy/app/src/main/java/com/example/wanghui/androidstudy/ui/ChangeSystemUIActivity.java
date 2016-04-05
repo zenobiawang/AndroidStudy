90120bc509c54aec0e31412e1829de602a7a2605
zenobia <974670990@qq.com>
2016-04-05 19:00:10 +0800

    利用ViewPropertyAniamtor实现简单动画

--- androidStudy/app/src/main/java/com/example/wanghui/androidstudy/ui/ChangeSystemUIActivity.java
+++ androidStudy/app/src/main/java/com/example/wanghui/androidstudy/ui/ChangeSystemUIActivity.java
@@ -12,19 +12,21 @@ import com.example.wanghui.androidstudy.R;
  */
 public class ChangeSystemUIActivity extends FragmentActivity {
     private int mTest1;
+    private BullsEyeView mBullsEyeView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_changesystemui);
+        mBullsEyeView = (BullsEyeView) findViewById(R.id.biv);
     }
 
     public void onToggleClick(View view){
-        int newVis;
+//        int newVis;
+//        newVis = getTest3();
+//        view.setSystemUiVisibility(newVis);
 
-        newVis = getTest3();
-
-        view.setSystemUiVisibility(newVis);
+        test4();
     }
 
 
@@ -38,6 +40,15 @@ public class ChangeSystemUIActivity extends FragmentActivity {
 
     private int getTest3(){   //全屏UI模式
         return View.SYSTEM_UI_FLAG_LAYOUT_STABLE|View.SYSTEM_UI_FLAG_FULLSCREEN|View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+    }
+
+    private void test4(){ //简单过渡动画
+        if (mBullsEyeView.getAlpha() > 0f){
+            mBullsEyeView.animate().alpha(0f).translationX(1000f).setDuration(500);
+        }else {
+            mBullsEyeView.setTranslationX(0f);
+            mBullsEyeView.animate().alpha(1f);
+        }
     }
 
 }
