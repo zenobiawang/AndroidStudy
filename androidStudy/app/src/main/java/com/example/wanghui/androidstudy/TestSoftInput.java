0b07868728f96e060f4d5c0eb00cfe9da5a37492
zenobia <974670990@qq.com>
2016-05-13 18:35:54 +0800

    输入框调研

---
+++ androidStudy/app/src/main/java/com/example/wanghui/androidstudy/TestSoftInput.java
@@ -0,0 +1,16 @@
+package com.example.wanghui.androidstudy;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by wanghui on 2016/5/13.
+ */
+public class TestSoftInput extends FragmentActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+    }
+}
