e6666b1180461a6d8aa554be6144021b70dce9fa
zenobia <974670990@qq.com>
2016-05-18 11:03:53 +0800

    多线程

--- androidStudy/app/src/main/java/com/example/wanghui/androidstudy/testrxjava/TestRxJavaActivity.java
+++ androidStudy/app/src/main/java/com/example/wanghui/androidstudy/testrxjava/TestRxJavaActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.wanghui.androidstudy.R;
@@ -25,15 +26,17 @@ import rx.functions.Func1;
  * http://www.tuicool.com/articles/BBNRRf
  */
 public class TestRxJavaActivity extends FragmentActivity {
+    Button mButton;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_rxa);
+        mButton = (Button) findViewById(R.id.test_click);
         setRxClick();
     }
 
     private void setRxClick() {
-        RxView.clicks(findViewById(R.id.test_click))
+        RxView.clicks(mButton)
                 .throttleFirst(3000, TimeUnit.MILLISECONDS)
                 .subscribe(new Action1<Void>() {
                     @Override
