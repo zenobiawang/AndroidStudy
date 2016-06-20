cb5cbc03069891dfcf121bd79c419e60ff42490e
zenobia <974670990@qq.com>
2016-06-20 18:12:19 +0800

    tablelayout

---
+++ androidStudy/app/src/main/java/com/example/wanghui/androidstudy/table/TableTestActivity.java
@@ -0,0 +1,17 @@
+package com.example.wanghui.androidstudy.table;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.wanghui.androidstudy.R;
+
+/**
+ * Created by wanghui on 2016/6/20.
+ */
+public class TableTestActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_table_test);
+    }
+}
