c8ecf5d7f46488bdd21355d95eaf3b6463ca9eb8
zenobiawang <974670990@qq.com>
2016-07-28 17:41:11 +0800

    listener animator

--- androidStudy/app/src/main/java/com/example/wanghui/androidstudy/animat/AnimationDemoAcivity.java
+++ androidStudy/app/src/main/java/com/example/wanghui/androidstudy/animat/AnimationDemoAcivity.java
@@ -4,10 +4,14 @@ import android.animation.Animator;
 import android.animation.AnimatorInflater;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
+import android.animation.IntEvaluator;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.LinearInterpolator;
+import android.widget.TextView;
 
 import com.example.wanghui.androidstudy.R;
 
@@ -16,6 +20,7 @@ import com.example.wanghui.androidstudy.R;
  */
 public class AnimationDemoAcivity extends Activity {
     private Object mObject;
+    private TextView mTv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,6 +59,22 @@ public class AnimationDemoAcivity extends Activity {
             }
         });
         animator.start();
+    }
+
+    private void demo5(){
+        ValueAnimator animator = ValueAnimator.ofInt(0, 100);
+        animator.setInterpolator(new AccelerateDecelerateInterpolator());
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            private IntEvaluator evaluator = new IntEvaluator();
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                // TODO: 16/7/28 1,根据时间流逝的比例得到属性值改变的比例 2,根据属性改变比例计算改变后的属性值 3,设置属性值
+                float fraction = animation.getAnimatedFraction();  //动画已执行的百分比
+                mTv.getLayoutParams().width = evaluator.evaluate(fraction, 0, 500);
+                mTv.requestLayout();
+            }
+        });
+        animator.setDuration(500).start();
     }
 
 }
