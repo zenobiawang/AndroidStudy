01752bdb59b03e2f994d6a060d7cb53872e22ce8
zenobia <974670990@qq.com>
2016-08-08 18:06:48 +0800

    surface

--- androidStudy/app/src/main/java/com/example/wanghui/androidstudy/view/PorterduffDemoActivity.java
+++ androidStudy/app/src/main/java/com/example/wanghui/androidstudy/view/PorterduffDemoActivity.java
@@ -1,7 +1,13 @@
 package com.example.wanghui.androidstudy.view;
 
 import android.app.Activity;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.graphics.SurfaceTexture;
 import android.os.Bundle;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
 import com.example.wanghui.androidstudy.R;
 
@@ -15,5 +21,78 @@ public class PorterduffDemoActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_porterduff);
+    }
+
+    private void test(){
+        SurfaceTexture texture = new SurfaceTexture(1);
+        Surface surface = new Surface(texture);
+        SurfaceHolder holder = new SurfaceHolder() {
+            @Override
+            public void addCallback(Callback callback) {
+
+            }
+
+            @Override
+            public void removeCallback(Callback callback) {
+
+            }
+
+            @Override
+            public boolean isCreating() {
+                return false;
+            }
+
+            @Override
+            public void setType(int type) {
+
+            }
+
+            @Override
+            public void setFixedSize(int width, int height) {
+
+            }
+
+            @Override
+            public void setSizeFromLayout() {
+
+            }
+
+            @Override
+            public void setFormat(int format) {
+
+            }
+
+            @Override
+            public void setKeepScreenOn(boolean screenOn) {
+
+            }
+
+            @Override
+            public Canvas lockCanvas() {
+                return null;
+            }
+
+            @Override
+            public Canvas lockCanvas(Rect dirty) {
+                return null;
+            }
+
+            @Override
+            public void unlockCanvasAndPost(Canvas canvas) {
+
+            }
+
+            @Override
+            public Rect getSurfaceFrame() {
+                return null;
+            }
+
+            @Override
+            public Surface getSurface() {
+                return null;
+            }
+        };
+        SurfaceView surfaceView = new SurfaceView(this);
+        surfaceView.getHolder();
     }
 }
