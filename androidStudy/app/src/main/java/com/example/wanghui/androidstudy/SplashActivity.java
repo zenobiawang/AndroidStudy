b72d18fde32726e70b42333817a5b99b77103937
zenobia <974670990@qq.com>
2016-08-12 16:23:20 +0800

    消息机制

--- androidStudy/app/src/main/java/com/example/wanghui/androidstudy/SplashActivity.java
+++ androidStudy/app/src/main/java/com/example/wanghui/androidstudy/SplashActivity.java
@@ -2,6 +2,7 @@ package com.example.wanghui.androidstudy;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.IdRes;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentActivity;
--- androidStudy/app/src/main/java/com/example/wanghui/androidstudy/TryCatchTestActivity.java
+++ androidStudy/app/src/main/java/com/example/wanghui/androidstudy/TryCatchTestActivity.java
@@ -2,15 +2,24 @@ package com.example.wanghui.androidstudy;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 
 /**
  * Created by wanghui on 2016/7/14.
  */
 public class TryCatchTestActivity extends Activity {
+    private Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mHandler.obtainMessage(1, "haha");
         try {
             throw new RuntimeException();
         }
