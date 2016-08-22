09ada1a958f487051b14e51af031f369f06bfd6e
zenobia <974670990@qq.com>
2016-08-22 16:51:30 +0800

    http://blog.csdn.net/lmj623565791/article/details/52204039

--- androidStudy/app/src/main/java/com/example/wanghui/androidstudy/view/SlideDesignLinearLayout.java
+++ androidStudy/app/src/main/java/com/example/wanghui/androidstudy/view/SlideDesignLinearLayout.java
@@ -5,10 +5,16 @@ import android.content.Context;
 import android.os.Build;
 import android.support.v4.view.NestedScrollingParent;
 import android.support.v4.view.ViewCompat;
+import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
+import android.view.VelocityTracker;
 import android.view.View;
+import android.view.ViewConfiguration;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.OverScroller;
 
+import com.example.wanghui.androidstudy.R;
 import com.example.wanghui.androidstudy.base.Log;
 
 /**
@@ -17,10 +23,6 @@ import com.example.wanghui.androidstudy.base.Log;
 public class SlideDesignLinearLayout extends LinearLayout implements NestedScrollingParent{
     public SlideDesignLinearLayout(Context context) {
         super(context);
-    }
-
-    public SlideDesignLinearLayout(Context context, AttributeSet attrs) {
-        super(context, attrs);
     }
 
     public SlideDesignLinearLayout(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -32,15 +34,235 @@ public class SlideDesignLinearLayout extends LinearLayout implements NestedScrol
         super(context, attrs, defStyleAttr, defStyleRes);
     }
 
+    private static final String TAG = "StickyNavLayout";
+
     @Override
-    public boolean onStartNestedScroll(View child, View target, int nestedScrollAxes) {
-        Log.d("wh-----onStartNestedScroll");
-        return (nestedScrollAxes & ViewCompat.SCROLL_AXIS_VERTICAL) != 0;
+    public boolean onStartNestedScroll(View child, View target, int nestedScrollAxes)
+    {
+        Log.d("wh---onStartNestedScroll");
+        return true;
     }
 
     @Override
-    public void onNestedPreScroll(View target, int dx, int dy, int[] consumed) {
-        Log.d("wh-----onNestedPreScroll");
-//        boolean hiddenTop = dy > 0 && getScrollY() <
+    public void onNestedScrollAccepted(View child, View target, int nestedScrollAxes)
+    {
+        Log.d("wh---onNestedScrollAccepted");
+    }
+
+    @Override
+    public void onStopNestedScroll(View target)
+    {
+        Log.d("wh---onStopNestedScroll");
+    }
+
+    @Override
+    public void onNestedScroll(View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed)
+    {
+        Log.d("wh---onNestedScroll");
+    }
+
+    @Override
+    public void onNestedPreScroll(View target, int dx, int dy, int[] consumed)
+    {
+        Log.d("wh---onNestedPreScroll");
+        boolean hiddenTop = dy > 0 && getScrollY() < mTopViewHeight;
+        boolean showTop = dy < 0 && getScrollY() >= 0 && !ViewCompat.canScrollVertically(target, -1);
+
+        if (hiddenTop || showTop)
+        {
+            scrollBy(0, dy);
+            consumed[1] = dy;
+        }
+    }
+
+    @Override
+    public boolean onNestedFling(View target, float velocityX, float velocityY, boolean consumed)
+    {
+        Log.d("wh---onNestedFling");
+        return false;
+    }
+
+    @Override
+    public boolean onNestedPreFling(View target, float velocityX, float velocityY)
+    {
+        Log.d("wh---onNestedPreFling");
+        //down - //up+
+        if (getScrollY() >= mTopViewHeight) return false;
+        fling((int) velocityY);
+        return true;
+    }
+
+    @Override
+    public int getNestedScrollAxes()
+    {
+        Log.d("wh---getNestedScrollAxes");
+        return 0;
+    }
+
+    private View mTop;
+    private View mNav;
+    private ViewPager mViewPager;
+
+    private int mTopViewHeight;
+
+    private OverScroller mScroller;
+    private VelocityTracker mVelocityTracker;
+    private int mTouchSlop;
+    private int mMaximumVelocity, mMinimumVelocity;
+
+    private float mLastY;
+    private boolean mDragging;
+
+    public SlideDesignLinearLayout(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+        setOrientation(LinearLayout.VERTICAL);
+
+        mScroller = new OverScroller(context);
+        mTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
+        mMaximumVelocity = ViewConfiguration.get(context)
+                .getScaledMaximumFlingVelocity();
+        mMinimumVelocity = ViewConfiguration.get(context)
+                .getScaledMinimumFlingVelocity();
+
+    }
+
+    private void initVelocityTrackerIfNotExists()
+    {
+        if (mVelocityTracker == null)
+        {
+            mVelocityTracker = VelocityTracker.obtain();
+        }
+    }
+
+    private void recycleVelocityTracker()
+    {
+        if (mVelocityTracker != null)
+        {
+            mVelocityTracker.recycle();
+            mVelocityTracker = null;
+        }
+    }
+
+
+//    @Override
+//    public boolean onTouchEvent(MotionEvent event)
+//    {
+//        initVelocityTrackerIfNotExists();
+//        mVelocityTracker.addMovement(event);
+//        int action = event.getAction();
+//        float y = event.getY();
+//
+//        switch (action)
+//        {
+//            case MotionEvent.ACTION_DOWN:
+//                if (!mScroller.isFinished())
+//                    mScroller.abortAnimation();
+//                mLastY = y;
+//                return true;
+//            case MotionEvent.ACTION_MOVE:
+//                float dy = y - mLastY;
+//
+//                if (!mDragging && Math.abs(dy) > mTouchSlop)
+//                {
+//                    mDragging = true;
+//                }
+//                if (mDragging)
+//                {
+//                    scrollBy(0, (int) -dy);
+//                }
+//
+//                mLastY = y;
+//                break;
+//            case MotionEvent.ACTION_CANCEL:
+//                mDragging = false;
+//                recycleVelocityTracker();
+//                if (!mScroller.isFinished())
+//                {
+//                    mScroller.abortAnimation();
+//                }
+//                break;
+//            case MotionEvent.ACTION_UP:
+//                mDragging = false;
+//                mVelocityTracker.computeCurrentVelocity(1000, mMaximumVelocity);
+//                int velocityY = (int) mVelocityTracker.getYVelocity();
+//                if (Math.abs(velocityY) > mMinimumVelocity)
+//                {
+//                    fling(-velocityY);
+//                }
+//                recycleVelocityTracker();
+//                break;
+//        }
+//
+//        return super.onTouchEvent(event);
+//    }
+
+
+    @Override
+    protected void onFinishInflate()
+    {
+        super.onFinishInflate();
+        mTop = findViewById(R.id.id_stickynavlayout_topview);
+        mNav = findViewById(R.id.id_stickynavlayout_indicator);
+        View view = findViewById(R.id.id_stickynavlayout_viewpager);
+        if (!(view instanceof ViewPager))
+        {
+            throw new RuntimeException(
+                    "id_stickynavlayout_viewpager show used by ViewPager !");
+        }
+        mViewPager = (ViewPager) view;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
+    {
+        //不限制顶部的高度
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        getChildAt(0).measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+        ViewGroup.LayoutParams params = mViewPager.getLayoutParams();
+        params.height = getMeasuredHeight() - mNav.getMeasuredHeight();
+        setMeasuredDimension(getMeasuredWidth(), mTop.getMeasuredHeight() + mNav.getMeasuredHeight() + mViewPager.getMeasuredHeight());
+
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh)
+    {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mTopViewHeight = mTop.getMeasuredHeight();
+    }
+
+
+    public void fling(int velocityY)
+    {
+        mScroller.fling(0, getScrollY(), 0, velocityY, 0, 0, 0, mTopViewHeight);
+        invalidate();
+    }
+
+    @Override
+    public void scrollTo(int x, int y)
+    {
+        if (y < 0)
+        {
+            y = 0;
+        }
+        if (y > mTopViewHeight)
+        {
+            y = mTopViewHeight;
+        }
+        if (y != getScrollY())
+        {
+            super.scrollTo(x, y);
+        }
+    }
+
+    @Override
+    public void computeScroll()
+    {
+        if (mScroller.computeScrollOffset())
+        {
+            scrollTo(0, mScroller.getCurrY());
+            invalidate();
+        }
     }
 }
