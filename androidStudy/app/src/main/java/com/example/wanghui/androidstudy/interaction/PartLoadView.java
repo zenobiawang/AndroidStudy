5d483c62bd768bbb9c4e3337a4770bc707ef269e
zenobia <974670990@qq.com>
2016-09-12 17:28:04 +0800

    局部加载控件

--- androidStudy/app/src/main/java/com/example/wanghui/androidstudy/interaction/PartLoadView.java
+++ androidStudy/app/src/main/java/com/example/wanghui/androidstudy/interaction/PartLoadView.java
@@ -2,6 +2,7 @@ package com.example.wanghui.androidstudy.interaction;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.graphics.BitmapFactory;
 import android.graphics.BitmapRegionDecoder;
 import android.graphics.Canvas;
 import android.os.Build;
@@ -9,12 +10,16 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 /**
  * Created by wanghui on 2016/9/8.
  * 局部加载
  */
 public class PartLoadView extends View {
     private BitmapRegionDecoder mDecoder;
+    private Context mContext;
 
     public PartLoadView(Context context) {
         super(context);
@@ -35,8 +40,31 @@ public class PartLoadView extends View {
     }
 
     private void init(Context context){
-
+        mContext = context;
     }
+
+    public void setInputStream(InputStream is){
+        try {
+            // TODO: 2016/9/12 首次展示
+            mDecoder = BitmapRegionDecoder.newInstance(is, false);
+            BitmapFactory.Options tempOptions = new BitmapFactory.Options();
+            tempOptions.inJustDecodeBounds = false;
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // TODO: 2016/9/12 获取屏幕尺寸
+    private int getPhoneWidth(){
+        return 1;
+    }
+    // TODO: 2016/9/12 获取屏幕尺寸
+    private int getPhoneHeight(){
+        return 1;
+    }
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
