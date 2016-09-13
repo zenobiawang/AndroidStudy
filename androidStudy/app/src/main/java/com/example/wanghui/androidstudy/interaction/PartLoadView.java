ac314064ad185f4b6c2231b46400d36d29719b66
zenobia <974670990@qq.com>
2016-09-13 15:42:44 +0800

    局部加载控件

--- androidStudy/app/src/main/java/com/example/wanghui/androidstudy/interaction/PartLoadView.java
+++ androidStudy/app/src/main/java/com/example/wanghui/androidstudy/interaction/PartLoadView.java
@@ -48,12 +48,24 @@ public class PartLoadView extends View {
             // TODO: 2016/9/12 首次展示
             mDecoder = BitmapRegionDecoder.newInstance(is, false);
             BitmapFactory.Options tempOptions = new BitmapFactory.Options();
-            tempOptions.inJustDecodeBounds = false;
 
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    static int findBestSampleSize(
+            int actualWidth, int actualHeight, int desiredWidth, int desiredHeight) {
+        double wr = (double) actualWidth / desiredWidth;
+        double hr = (double) actualHeight / desiredHeight;
+        double ratio = Math.min(wr, hr);
+        float n = 1.0f;
+        while ((n * 2) <= ratio) {
+            n *= 2;
+        }
+
+        return (int) n;
     }
 
     // TODO: 2016/9/12 获取屏幕尺寸
